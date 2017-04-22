02d3532cd3164e68da52a93685908b9906ec5e43
simonihmig <ihmig@kaliber5.de>
2017-04-23 00:40:33 +0200

    Split to separate transforms, added length transform

---
+++ test/expected-output/length.js
@@ -0,0 +1,13 @@
+import { findAll } from 'ember-native-dom-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('foo-bar', 'Integration | Component | foo bar', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  assert.equal(findAll('.foo').length, 1);
+});
---
+++ test/expected-output/text.js
@@ -0,0 +1,13 @@
+import { find } from 'ember-native-dom-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('foo-bar', 'Integration | Component | foo bar', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  assert.equal(find('.foo').textContent.trim(), '');
+});
