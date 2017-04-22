c15b923f8e96a0b970e56dc04254be87762c1355
simonihmig <ihmig@kaliber5.de>
2017-04-23 01:04:49 +0200

    has-class

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const textTransform = require('./lib/transforms/text');
 const lengthTransform = require('./lib/transforms/length');
+const hasClassTransform = require('./lib/transforms/has-class');
 
 const transforms = [
   textTransform,
-  lengthTransform
+  lengthTransform,
+  hasClassTransform
 ];
 
 module.exports = function(file, api, options) {
