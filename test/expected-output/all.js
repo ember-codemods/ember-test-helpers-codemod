a53379e290f4d2a665e616dbe6da595fc87bbb3d
simonihmig <ihmig@kaliber5.de>
2017-04-27 10:48:47 +0200

    added all test, fix import

---
+++ test/expected-output/all.js
@@ -0,0 +1,32 @@
+import { click, find, findAll, fillIn, triggerEvent } from 'ember-native-dom-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('foo-bar', 'Integration | Component | foo bar', {
+  integration: true
+});
+
+test('it renders', async function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  await click('.foo');
+  assert.equal(find('.foo').getAttribute('id'), 'foo');
+  await fillIn('.foo input', 'bar');
+  await triggerEvent('.foo input', 'change');
+  assert.equal(find('.foo').textContent.trim(), 'foo');
+});
+
+test('it renders again', function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  assert.equal(findAll('.foo input').length, 1);
+  assert.equal(find('.foo input').value, 'foo');
+  assert.ok(find('.foo').classList.contains('selected'));
+});
+
+test('and again', async function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  await click('foo');
+  assert.ok(find('.foo').classList.contains('selected'));
+});
---
+++ test/input/all.js
@@ -0,0 +1,31 @@
+import { click } from 'ember-native-dom-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('foo-bar', 'Integration | Component | foo bar', {
+  integration: true
+});
+
+test('it renders', async function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  await click('.foo');
+  assert.equal(this.$('.foo').attr('id'), 'foo');
+  this.$('.foo input').val('bar').change();
+  assert.equal(this.$('.foo').text().trim(), 'foo');
+});
+
+test('it renders again', function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  assert.equal(this.$('.foo input').length, 1);
+  assert.equal(this.$('.foo input').val(), 'foo');
+  assert.ok(this.$('.foo').hasClass('selected'));
+});
+
+test('and again', function(assert) {
+  this.render(hbs`{{foo-bar}}`);
+
+  this.$('foo').click();
+  assert.ok(this.$('.foo').hasClass('selected'));
+});
