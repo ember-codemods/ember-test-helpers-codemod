35878dfd5d6acd7a11a7c204e154d794f230a670
Steve Calvert <scalvert@linkedin.com>
2017-11-03 08:29:58 -0700

    Adding failing test for when selectors are variables

--- __testfixtures__/integration/click.input.js
+++ __testfixtures__/integration/click.input.js
@@ -1,6 +1,8 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
+const SELECTOR_FIRST = '.foo:first';
+
 moduleForComponent('foo-bar', 'Integration | Component | foo bar', {
   integration: true
 });
@@ -11,5 +13,6 @@ test('it renders', function(assert) {
   this.$('.foo').click();
   this.$('.baz a:eq(0)').click();
   this.$('.foo .bar:eq(3)').click();
+  this.$(SELECTOR_FIRST).click();
   assert.ok(true);
 });
--- __testfixtures__/integration/click.output.js
+++ __testfixtures__/integration/click.output.js
@@ -2,6 +2,8 @@ import { findAll, click } from 'ember-native-dom-helpers';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
+const SELECTOR_FIRST = '.foo:first';
+
 moduleForComponent('foo-bar', 'Integration | Component | foo bar', {
   integration: true
 });
@@ -12,5 +14,6 @@ test('it renders', async function(assert) {
   await click('.foo');
   await click('.baz a');
   await click(findAll('.foo .bar')[3]);
+  this.$(SELECTOR_FIRST).click();
   assert.ok(true);
 });
