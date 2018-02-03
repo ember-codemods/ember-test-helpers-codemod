8b478f66b08ab4f2046652841398d553e240aca6
simonihmig <ihmig@kaliber5.de>
2018-02-03 01:20:17 +0100

    Added tests for ember-native-dom-helpers transform

---
+++ __testfixtures__/acceptance/native-dom.input.js
@@ -0,0 +1,21 @@
+import { currentURL, currentPath, currentRouteName, find, visit } from 'ember-native-dom-helpers';
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('click');
+
+test('visiting /foo', async function(assert) {
+  await visit('/foo');
+  assert.equal(currentURL(), '/foo');
+  assert.ok(find('.foo'));
+});
+
+test('visiting /bar', async function(assert) {
+  await visit('/bar');
+  assert.equal(currentPath(), 'bar');
+});
+
+test('visiting /bar', async function(assert) {
+  await visit('/bar');
+  assert.equal(currentRouteName(), 'bar.index');
+});
