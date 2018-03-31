767ec6c698188da203bd4a27b65ba35c3edea274
simonihmig <ihmig@kaliber5.de>
2018-03-31 17:10:50 +0200

    Add test for bin command

--- __testfixtures__/find.output.js
+++ __testfixtures__/find.output.js
@@ -1,4 +1,3 @@
-
 test('it renders', function(assert) {
   assert.equal(this.element.querySelector('.foo').textContent.trim(), 'bar');
   assert.equal(this.element.querySelectorAll('.bar').length, 2);
