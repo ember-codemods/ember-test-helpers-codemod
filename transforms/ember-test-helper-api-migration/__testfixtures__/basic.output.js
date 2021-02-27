5ab3066f2673c4726124da8b47feb2eeb4835d86
Zhan Wang <zhanwang@zhanwang-mn1.linkedin.biz>
2021-02-27 15:05:13 -0800

    update testfixtures to fit new util.js

--- transforms/ember-test-helper-api-migration/__testfixtures__/basic.output.js
+++ transforms/ember-test-helper-api-migration/__testfixtures__/basic.output.js
@@ -1,2 +1,7 @@
 import { setApplication, setResolver } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+
+setResolver(engineResolverFor('shared-components'));
+
+setApplication(Application.create(config.APP));
+preloadAssets(manifest).then(start);
--- transforms/ember-test-helper-api-migration/__testfixtures__/do-not-have-@ember-test-helpers-import.input.js
+++ transforms/ember-test-helper-api-migration/__testfixtures__/do-not-have-@ember-test-helpers-import.input.js
@@ -1,2 +1,7 @@
 import { start } from 'ember-qunit';
 import { setResolver } from 'ember-test-helpers';
+
+setResolver(engineResolverFor('shared-components')); 
+ 
+setApplication(Application.create(config.APP)); 
+preloadAssets(manifest).then(start); 
