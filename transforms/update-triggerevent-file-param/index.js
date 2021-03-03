a744fc2dc1196395ece54b4c94682baf1fcdcc32
Zhan Wang <zhanwang@zhanwang-mn1.linkedin.biz>
2021-03-03 00:36:36 -0800

    fix lint err

--- transforms/update-triggerevent-file-param/index.js
+++ transforms/update-triggerevent-file-param/index.js
@@ -8,16 +8,22 @@ module.exports = function transformer(file, api) {
    * A transform to update param in `triggerEvent` from `[ file ]` to `{ files: [ file ] }`
    */
   function transform() {
-    root.find(j.CallExpression, {
-      callee: {
-        name: 'triggerEvent'
-      }
-    }).find(j.ArrayExpression).replaceWith(path => {
-      // make sure we just modify the arrayExpression which is the param of triggerEvent call expression
-      if (path.parent.node.type === 'CallExpression' && path.parent.node.callee.name === 'triggerEvent'){
-        return j.objectExpression([j.property('init',j.identifier('files'), path.node)])
-      } else return path.node
-    })
+    root
+      .find(j.CallExpression, {
+        callee: {
+          name: 'triggerEvent',
+        },
+      })
+      .find(j.ArrayExpression)
+      .replaceWith((path) => {
+        // make sure we just modify the arrayExpression which is the param of triggerEvent call expression
+        if (
+          path.parent.node.type === 'CallExpression' &&
+          path.parent.node.callee.name === 'triggerEvent'
+        ) {
+          return j.objectExpression([j.property('init', j.identifier('files'), path.node)]);
+        } else return path.node;
+      });
   }
 
   transform();
@@ -26,4 +32,4 @@ module.exports = function transformer(file, api) {
     quote: 'single',
     trailingComma: false,
   });
-}
+};
